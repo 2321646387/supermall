bdc59ca19d57a35d2a61658eff2c43ec255b6f2e
2321646387 <2321646387@qq.com>
2021-06-21 16:19:16 +0800

    first comm

---
+++ src/router/index.js
@@ -0,0 +1,40 @@
+import { createRouter, createWebHistory } from "vue-router";
+// import Home from '../views/Home.vue'
+const Home = () => import("../views/home/Home");
+const Category = () => import("../views/category/Category");
+const Cart = () => import("../views/cart/Cart");
+const Profile = () => import("../views/profile/Profile");
+
+const routes = [
+  {
+    path: "",
+    redirect: "/home"
+  },
+  {
+    path: "/home",
+    // name: "Home",
+    component: Home
+  },
+  {
+    path: "/category",
+    // name: "Category",
+    component: Category
+  },
+  {
+    path: "/cart",
+    // name: "Cart",
+    component: Cart
+  },
+  {
+    path: "/profile",
+    // name: "Profile",
+    component: Profile
+  }
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+});
+
+export default router;
